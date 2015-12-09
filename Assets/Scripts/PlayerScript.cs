f428aeb3a9286020e3c11e760fdfe990f660a4ab
tblanchet <tblanchet@switchfly.com>
2015-12-09 23:57:31 +0000

    added first assets, made ship move

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector2 speed = new Vector2(50,50);
+	private Vector2 movement;
+
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector2 (
+			speed.x * inputX,
+			speed.y * inputY);
+	
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
