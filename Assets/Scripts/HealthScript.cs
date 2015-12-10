9c1cd6cf63a384c86392aa31d0c5d488dfd6a01a
tblanchet <tblanchet@switchfly.com>
2015-12-10 00:43:35 +0000

    can shoot now

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	public int hp = 1;
+	public bool isEnemy = true;
+
+	public void Damage(int damageCount) {
+		hp -= damageCount;
+
+		if (hp <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript> ();
+		if (shot != null) {
+			if (shot.isEnemyShot != isEnemy) {
+				Damage (shot.damage);
+				Destroy (shot.gameObject);
+			}
+		}
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,6 +15,16 @@ public class PlayerScript : MonoBehaviour {
 		movement = new Vector2 (
 			speed.x * inputX,
 			speed.y * inputY);
+
+		bool shoot = Input.GetButtonDown ("Fire1");
+		shoot |= Input.GetButtonDown ("Fire2");
+
+		if (shoot) {
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if(weapon != null) {
+				weapon.Attack(false);
+			}
+		}
 	
 	}
 
