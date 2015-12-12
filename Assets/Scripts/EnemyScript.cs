e62fd6a79cfea9b33c5c52e9255db7eafd810551
tblanchet <tblanchet@switchfly.com>
2015-12-12 22:31:33 +0000

    added enemy attacking and dying

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	private WeaponScript[] weapons;
+
+	void Awake() {
+		weapons = GetComponentsInChildren<WeaponScript> ();
+	}
+
+	void Update() {
+		foreach (WeaponScript weapon in weapons) {
+			if (weapon != null && weapon.CanAttack) {
+				weapon.Attack (true);
+			}
+		}
+	}
+}
